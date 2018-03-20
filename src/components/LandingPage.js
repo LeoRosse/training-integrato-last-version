f49ed77cfa955b589ce15a1e785b728fd9b15bba
leorosse <leorosse87@gmail.com>
2018-03-20 13:14:37 +0100

    new landing grapich

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const LandingPage = () => (
-    <div className="box-layout-landing">
-        <div className="box-layout-landing__box">
-            <h1 className="box-layout-landing__title">Training Integrato</h1>
-            <p>Benvenuto nel sito di Alberto Santarato</p>
-            <Link className="button__landing" to="/dashboard-public">Entra</Link>
-            <Link className="button__landing" to="/login">Login</Link>
-            </div>
+    /*     <div classNameName="box-layout-landing">
+            <div classNameName="box-layout-landing__box">
+                <h1 classNameName="box-layout-landing__title">Training Integrato</h1>
+                <p>Benvenuto nel sito di Alberto Santarato</p>
+                <Link classNameName="button__landing" to="/dashboard-public">Entra</Link>
+                <Link classNameName="button__landing" to="/login">Login</Link>
+                </div>
+        </div> */
+    <div className="landing">
+        <div className="logo-box">
+            {/* <img src="/images/logo-1.jpg" alt="Logo" className="logo" /> */}
+        </div>
+
+        <div className="text-box">
+            <h1 className="heading-primary">
+                <span className="heading-primary-main">Training Integrato</span>
+                <span className="heading-primary-sub">Alberto Santarato</span>
+            </h1>
+
+            <Link className="btn btn-white btn-animated" to="/dashboard-public">Entra</Link>
+        </div>
+
     </div>
 );
 
