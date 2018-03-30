093345052fb7c92110440d505dcd45b5a2095ef0
leorosse <leorosse87@gmail.com>
2018-03-30 21:18:22 +0200

    modified config in firebase js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyBkOh3VxQideB6jq3naWdBSV6QxLxeN6Uo',
+  authDomain: 'training-integrato.firebaseapp.com',
+  databaseURL: 'https://training-integrato.firebaseio.com',
+  projectId: 'training-integrato',
+  storageBucket: 'training-integrato.appspot.com',
+  messagingSenderId: '323414795114'
 };
 
 firebase.initializeApp(config);
