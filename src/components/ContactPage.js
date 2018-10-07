93a0ab8bbbe2753b278ab4c12fc0124c03a64f62
leorosse <leorosse87@gmail.com>
2018-10-07 19:50:59 +0200

    insert a in contact number

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,19 +6,19 @@ const ContactPage = () => (
     <div>
         <Navigation />
         <div className="contact">
-                <div className="bg-video">
-                    <img src="images/contact-web.jpg" alt="contact" className="bg-video__content" />
+            <div className="bg-video">
+                <img src="images/contact-web.jpg" alt="contact" className="bg-video__content" />
+            </div>
+            <div className="row">
+                <div className="contact__content">
+                    <ul className="contact__list">
+                        <li className="contact__item__top">Puoi contattarmi qui:</li>
+                        <a href="tel:+39 333 97 60 998"><li className="contact__item mobilesOnly">+39 333 97 60 998</li></a>
+                        <li className="contact__item desktopOnly">+39 333 97 60 998</li>
+                        <li className="contact__item contact__item--email">alberto.santarato@gmail.com</li>
+                    </ul>
                 </div>
-                <div className="row">
-                    <div className="contact__content">
-                        <ul className="contact__list">
-                            <li className="contact__item__top">Puoi contattarmi qui:</li>
-                            <li className="contact__item mobilesOnly" href="tel:393339760998">+39 333 97 60 998</li>
-                            <li className="contact__item desktopOnly">+39 333 97 60 998</li>
-                            <li className="contact__item contact__item--email">alberto.santarato@gmail.com</li>
-                        </ul>
-                    </div> 
-                </div>    
+            </div>
         </div>
         <Footer />
     </div>
