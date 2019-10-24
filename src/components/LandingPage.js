f52d34ba32b9ae9ad39c8fd710c51e0908d5831e
Leonardo Rosseti <rosseti@extendi.it>
2019-10-24 11:44:03 +0200

    clean code LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,77 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
-import Modal from "./UI/Modal";
-import moment from "moment";
+import Footer from "./Footer";
 
 class LandingPage extends Component {
-  state = {
-    showModal: false
-  };
-
-  modalCancelHandler = () => {
-    this.setState({ showModal: false });
-  };
-
-  modalHandler = () => {
-    this.setState({ showModal: true });
-  };
-
-  renderModal = () => {
-    return (
-      <Modal show={this.state.showModal} modalClosed={this.modalCancelHandler}>
-        <div className="popup__content">
-          <div className="popup__left">
-            <img
-              src="images/sport_popup-web.jpg"
-              alt="Sport"
-              className="popup__img"
-            />
-          </div>
-          <div className="popup__right">
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              <b>Percorso:</b>
-            </h3>
-            <div className="paragraph">
-              <ul className="paragraph__list">
-                <li className="paragraph__item">Colloquio Iniziale</li>
-                <li className="paragraph__item">Studio della costituzione</li>
-                <li className="paragraph__item">Analisi BIA</li>
-                <li className="paragraph__item">Educazione Alimentare</li>
-              </ul>
-            </div>
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              <b>Al termine della consulenza vengono indicati:</b>
-            </h3>
-            <div className="paragraph">
-              <ul className="paragraph__list">
-                <li className="paragraph__item">
-                  Consigli per un sano stile di vita
-                </li>
-                <li className="paragraph__item">
-                  Regole generali per una corretta alimentazione
-                </li>
-                <li className="paragraph__item">
-                  Suggerimenti di tecniche da praticare a casa
-                </li>
-                <li className="paragraph__item">
-                  Indicazione per una adeguata attività fisica
-                </li>
-              </ul>
-            </div>
-            <a
-              href="/contact"
-              className="btn btn--green"
-              style={{ float: "right" }}
-            >
-              Contatta
-            </a>
-          </div>
-        </div>
-      </Modal>
-    );
-  };
-
   render() {
     return (
       <div>
@@ -92,24 +24,6 @@ class LandingPage extends Component {
               </h2>
             </div>
             <div className="row">
-              {/*                            <p className="paragraph">
-                                E’ un percorso dedicato al raggiungimento e al
-                                mantenimento di un buono stato di salute e di forma fisica.
-                                Le fondamenta si basano su un corretto stile di vita, su una
-                                alimentazione consapevole e su una attività fisica mirata.
-                                Lo stato di salute è fusione, equilibrio e armonia tra le varie
-                                componenti essenziali del corpo.
-                                Il colloquio con la persona viene indirizzato in modo mirato
-                                per conoscere le peculiarità dell’individuo, anche attraverso
-                                domande specifiche che vanno ad evidenziare alcuni
-                                sintomi e le sue caratteristiche psico-fisiche, per valutarne
-                                la costituzione ed individuare le tipologie di intervento.
-                                Il colloquio viene integrato con una valutazione della
-                                composizione corporea (BIA) che permette di valutare
-                                lo stato nutrizionale della persona per modulare
-                                eventuali squilibri alimentari e sviluppare un programma
-                                di attività fisica mirata.
-        </p>*/}
               <p className="paragraph">
                 <i>
                   Il metodo Training Integrato inizia da un accurato studio
@@ -139,15 +53,6 @@ class LandingPage extends Component {
                   a capire in modo scientifico come cambiano nel tempo, per
                   avere chiare indicazioni su come raggiungere i propri
                   obbiettivi di salute e di performance.
-                  {/*                                <br />
-                                    <br />
-                                    Attraverso la <b>metodica BIA</b> siamo in grado di analizzare:
-                                <ul className="feature-box__list">
-                                        <li>lo stato di idratazione</li>
-                                        <li>valutare la quantità di muscolo e grasso corporeo</li>
-                                        <li>stimare il metabolismo basale</li>
-                                        <li>analizzare lo stato di stress cellulare</li>
-</ul>*/}
                 </span>
                 <br />
                 <br />
@@ -182,12 +87,7 @@ class LandingPage extends Component {
                     </div>
                   </Link>
                   <div className="card__side card__side--back card__side--back-3">
-                    <div className="card__cta">
-                      {/*                                     <div className="card__price-box">
-                                                <p className="card__price-value">€75</p>
-                                            </div> */}
-                      {/*<a href="#popup_2" className="btn btn--white">Scopri di più</a>*/}
-                    </div>
+                    <div className="card__cta"></div>
                   </div>
                 </div>
               </div>
@@ -212,12 +112,7 @@ class LandingPage extends Component {
                     </Link>
                   </div>
                   <div className="card__side card__side--back card__side--back-1">
-                    <div className="card__cta">
-                      {/*                                     <div className="card__price-box">
-                                            <p className="card__price-value">€50</p>
-                                        </div> */}
-                      {/*<a href="#popup_2" className="btn btn--white">Scopri di più</a>*/}
-                    </div>
+                    <div className="card__cta"></div>
                   </div>
                 </div>
               </div>
@@ -243,12 +138,7 @@ class LandingPage extends Component {
                     </Link>
                   </div>
                   <div className="card__side card__side--back card__side--back-2">
-                    <div className="card__cta">
-                      {/*                                     <div className="card__price-box">
-                                            <p className="card__price-value">€60</p>
-                                        </div> */}
-                      {/*<a href="#popup_2" className="btn btn--white">Scopri di più</a>*/}
-                    </div>
+                    <div className="card__cta"></div>
                   </div>
                 </div>
               </div>
@@ -306,128 +196,7 @@ class LandingPage extends Component {
                     </div>
                 </section> */}
         </main>
-        <footer className="footer">
-          <div className="footer__logo-box">
-            <img
-              src="images/Logo3dtrasp.png"
-              alt="Full Logo"
-              className="footer__logo"
-            />
-          </div>
-          <div className="row">
-            <div className="col-1-of-2">
-              <div className="footer__logo--box">
-                <a
-                  href="http://www.facebook.com/trainingintegrato.it"
-                  target="blank"
-                >
-                  <i className="fab fa-facebook-square icon" />
-                </a>
-                <a
-                  href="https://www.youtube.com/user/albertosantarato"
-                  target="blank"
-                >
-                  <i className="fab fa-youtube icon" />
-                </a>
-                <a
-                  href="https://www.instagram.com/trainingintegrato/"
-                  target="blank"
-                >
-                  <i className="fab fa-instagram icon" />
-                </a>
-              </div>
-              <div className="footer__copyright">
-                <ul className="footer__list">
-                  <li className="footer__item">
-                    <Link to="/chi-sono" className="footer__link">
-                      Partner
-                    </Link>
-                  </li>
-                  <li className="footer__item">
-                    <Link to="/contact" className="footer__link">
-                      Contattaci
-                    </Link>
-                  </li>
-                  <li className="footer__item">
-                    <Link to="/chi-sono" className="footer__link">
-                      Curriculum
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <p className="footer__copyright footer__copyright--noborder">
-                Si riceve presso:
-                <a
-                  href="https://www.google.com/maps/place/Fisioner+-+Studio+di+Fisioterapia+e+Riabilitazione/@43.7752199,11.2099584,17z/data=!3m1!4b1!4m5!3m4!1s0x132a56d47364f9ed:0x7e4e0c795e4f1e5e!8m2!3d43.775216!4d11.2121471"
-                  className="footer__link"
-                >
-                  <i class="fas fa-map-marker marker"></i> Fisioner
-                </a>
-                <br className="mobilesOnly" />
-                <a
-                  href="https://www.google.com/maps/place/C.M.O.+Centro+Medicina+Osteopatica+di+Andrea+Corti+e+Nicola+Barsotti/@43.7575831,11.292416,17z/data=!3m1!4b1!4m5!3m4!1s0x132a538251b085a1:0x66c2abfc38649820!8m2!3d43.7575792!4d11.2946047"
-                  className="footer__link"
-                >
-                  <i class="fas fa-map-marker marker"></i> Cmo Firenze
-                </a>
-              </p>
-            </div>
-
-            <div className="col-1-of-2">
-              <div className="footer__logo--box">
-                <a href="http://www.cnmitalia.com/">
-                  <img
-                    src="images/CNM-Logo.png"
-                    alt="CNM Logo"
-                    className="footer__logo__cnm"
-                  />
-                </a>
-                <a href="https://www.unifi.it">
-                  <img
-                    src="images/logo-unifi.png"
-                    alt="Unifi Logo"
-                    className="footer__logo__unifi"
-                  />
-                </a>
-                <a href="https://web.uniroma2.it/">
-                  <img
-                    src="images/logo-TorVergata.png"
-                    alt="CNM Logo"
-                    className="footer__logo__vergata"
-                  />
-                </a>
-              </div>
-              <p className="footer__copyright">
-                Built by{" "}
-                <a
-                  href="https://www.facebook.com/l.rosseti"
-                  className="footer__link"
-                >
-                  {" "}
-                  Leonardo Rosseti
-                </a>
-                ,<br className="mobilesOnly" /> Design by{" "}
-                <a
-                  href="http://codingheroes.io/resources/"
-                  className="footer__link"
-                >
-                  Jonas Schmedtmann
-                </a>
-              </p>
-              <p className="footer__copyright footer__copyright--noborder">
-                Copyright
-                <a
-                  href="http://www.trainingintegrato.com/"
-                  className="footer__link"
-                >
-                  © Training Integrato {moment().format("YYYY")}
-                </a>
-                <br className="mobilesOnly" />
-                All rights reserved.
-              </p>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     );
   }
